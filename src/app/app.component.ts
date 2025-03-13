7aa8ce6cbe3e4314f5cde5db85ea7e7186fbe3ed
Yashwanth <Yashwanth@LAPTOP-8UCC259I>
2025-03-13 00:00:09 -0400

    Created UI YashDatingApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any
+
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7065/api/Users').subscribe({
+      next: response => this.users = response,
+      error: error=> console.log(error),
+      complete: ()=> console.log('Request completed')
+    })
+  }
+  
 }
